4f34c0aa6b07d0847bdc41febaf945048dcd468c
Chiijlaw <chiijlaw@berkeley.edu>
2018-09-17 21:53:30 -0700

    Allows cross origin requests and refactored endpoint of GET request to look at the proper server.

--- client/components/ImagesList.jsx
+++ client/components/ImagesList.jsx
@@ -43,7 +43,7 @@ class ImagesList extends React.Component {
 
   componentDidMount() {
     const id = window.location.pathname.split('/')[1];
-    axios.get(`/${id}/api/images`).then(results => {
+    axios.get(`http://localhost:3001/${id}/api/images`).then(results => {
       this.setState({
         images: results.data
       });
